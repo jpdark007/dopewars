615b3a536dc0702cbe93d2848cd6642d42b74756
jpdark007 <dark_angel@laposte.net>
2017-02-16 10:49:24 +0100

    NEW FEATURE : Load data from xml

--- src/main/java/fr/game/dopewars/Game.java
+++ src/main/java/fr/game/dopewars/Game.java
@@ -2,8 +2,14 @@ package fr.game.dopewars;
 
 import fr.game.dopewars.events.*;
 import fr.game.dopewars.exceptions.*;
+
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.*;
 import java.util.Map.Entry;
+
+import com.thoughtworks.xstream.XStream;
 
 /**
  * Main dopewars game class. Holds state for the game
@@ -14,17 +20,25 @@ import java.util.Map.Entry;
 public final class Game implements TraderConstants {
 
 	static private Game instance = null;
-	final static private Product[] products = { new Product("Acid", 1000, 4500), new Product("Cocaine", 15000, 30000),
-			new Product("Hashish", 450, 1500), new Product("Heroin", 5000, 14000), new Product("Ecstacy", 10, 60),
-			new Product("Smack", 1500, 4500), new Product("Opium", 500, 1300), new Product("Crack", 1000, 3500),
-			new Product("Peyote", 100, 700), new Product("Shrooms", 600, 1400), new Product("Speed", 70, 250),
-			new Product("Weed", 300, 900) };
-	final static private Location[] locations = { new Location("Coney Island", products),
-			new Location("Manhattan", products), new Location("The Bronz", products), new Location("SoHo", products),
-			new Location("The Village", products) };
-	final static private Npc[] npcs = { new Npc("Street Thug", 20, 20, 5, 2000, 5000),
-			new Npc("Officer Hardass", 50, 25, 0, 10000, 20000), new Npc("Rival Dealer", 80, 25, 2, 50000, 75000, 2),
-			new Npc("Robocop", 300, 40, 0, 160000, 250000, 3) };
+
+	static private Product[] products;
+	// final static private Product[] products = { new Product("Acid", 1000,
+	// 4500), new Product("Cocaine", 15000, 30000),
+	// new Product("Hashish", 450, 1500), new Product("Heroin", 5000, 14000),
+	// new Product("Ecstacy", 10, 60),
+	// new Product("Smack", 1500, 4500), new Product("Opium", 500, 1300), new
+	// Product("Crack", 1000, 3500),
+	// new Product("Peyote", 100, 700), new Product("Shrooms", 600, 1400), new
+	// Product("Speed", 70, 250),
+	// new Product("Weed", 300, 900) };
+	 static private Location[] locations;
+//	final static private Location[] locations = { new Location("Coney Island", products),
+//			new Location("Manhattan", products), new Location("The Bronz", products), new Location("SoHo", products),
+//			new Location("The Village", products) };
+	 static private Npc[] npcs;
+//	final static private Npc[] npcs = { new Npc("Street Thug", 20, 20, 5, 2000, 5000),
+//			new Npc("Officer Hardass", 50, 25, 0, 10000, 20000), new Npc("Rival Dealer", 80, 25, 2, 50000, 75000, 2),
+//			new Npc("Robocop", 300, 40, 0, 160000, 250000, 3) };
 	private Player player;
 	private Location location;
 	private Trade currentTrade;
@@ -47,15 +61,69 @@ public final class Game implements TraderConstants {
 		messages = new ArrayList<Message>();
 		day = 0;
 
+		loadProducts();
+		loadLocations();
+		loadNpcs();
+
 		player = new Player("You");
 		location = locations[0];
 
 		setupEvents();
 		updatePrices();
 	}
+	
+	private static void loadNpcs() {
+		FileReader fileReader;
+		XStream xstream = new XStream();
+		xstream.alias("Npc", Npc.class);
+		try {
+			fileReader = new FileReader("npcs.xml");
+			npcs = (Npc[]) xstream.fromXML(fileReader);
+			fileReader.close();
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+
+	private static void loadLocations() {
+		FileReader fileReader;
+		XStream xstream = new XStream();
+		xstream.alias("Location", Location.class);
+		try {
+			fileReader = new FileReader("locations.xml");
+			List<Location> locationsLoad =  (List<Location>) xstream.fromXML(fileReader);
+			fileReader.close();
+			List<Location> locationsTmp = new ArrayList<Location>();
+			for (Location location : locationsLoad) {
+				locationsTmp.add(new Location(location.getName(), products));
+			}
+			locations = locationsTmp.toArray(new Location[locationsTmp.size()]);
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	private static void loadProducts() {
+		FileReader fileReader;
+		XStream xstream = new XStream();
+		xstream.alias("Product", Product.class);
+		try {
+			fileReader = new FileReader("products.xml");
+			products = (Product[]) xstream.fromXML(fileReader);
+			fileReader.close();
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
 
 	public void checkGameConditions() throws GameOverException {
-		boolean over = false;
 
 		if (day > DAYS_IN_GAME) {
 			throw new GameOverException("Time's up!");
@@ -132,9 +200,6 @@ public final class Game implements TraderConstants {
 
 	public void runGameEvents() {
 		updatePrices();
-		int r;
-		Product product;
-
 		for (Event e : gameEvents) {
 			if (e.inEvent()) {
 				events.add(e);
@@ -203,7 +268,7 @@ public final class Game implements TraderConstants {
 		System.out.print(player.getCashValue());
 		System.out.print(" Space : " + player.spaceHeld() + "/" + player.getSpace());
 		System.out.print(" Health : " + player.getHealth() + "/" + player.getMaxHealth());
-		System.out.print(" Days : "+ daysLeft() + "/" + Game.DAYS_IN_GAME);
+		System.out.print(" Days : " + daysLeft() + "/" + Game.DAYS_IN_GAME);
 		System.out.println();
 		System.out.println("Products : ");
 		for (Product product : getProducts()) {
@@ -213,7 +278,7 @@ public final class Game implements TraderConstants {
 		printStash();
 		System.out.println(getMessages());
 	}
-	
+
 	public void printStash() {
 		System.out.println("Stash : ");
 		Map<Product, Map<Long, Integer>> map = getPlayer().getProducts();
@@ -221,7 +286,8 @@ public final class Game implements TraderConstants {
 		for (Map.Entry<Product, Map<Long, Integer>> entry : map.entrySet()) {
 			Map<Long, Integer> stock = entry.getValue();
 			for (Entry<Long, Integer> oneProduct : stock.entrySet()) {
-				System.out.println( i++ + " / " + entry.getKey() + " Price :" + oneProduct.getKey() + "/ Quantity :" + oneProduct.getValue());
+				System.out.println(i++ + " / " + entry.getKey() + " Price :" + oneProduct.getKey() + "/ Quantity :"
+						+ oneProduct.getValue());
 			}
 		}
 	}
--- src/main/java/fr/game/dopewars/Product.java
+++ src/main/java/fr/game/dopewars/Product.java
@@ -4,70 +4,74 @@ package fr.game.dopewars;
  * Holds product state
  */
 public class Product implements TraderConstants {
-  final static int EVENT_RATE = 3;
-  final static int EVENT_CHANCE = 30;
+	final static int EVENT_RATE = 3;
+	final static int EVENT_CHANCE = 30;
 
-  protected String name;
-  protected int highPrice;
-  protected int lowPrice;
-  protected String lowMessage;
-  protected String highMessage;
-  protected int price;
-  protected Message message;
+	protected String name;
+	protected int highPrice;
+	protected int lowPrice;
+	protected String lowMessage;
+	protected String highMessage;
+	protected int price;
+	protected Message message;
 
-  public Product(String name, int lowPrice, int highPrice) {
-    this(name, lowPrice, highPrice, "%s prices have bottomed out!", "%s prices are sky high!");
-  }
+	public Product(String name, int lowPrice, int highPrice) {
+		this(name, lowPrice, highPrice, "%s prices have bottomed out!", "%s prices are sky high!");
+	}
 
-  public Product(String name, int lowPrice, int highPrice, String lowMessage, String highMessage) {
-    this.name = name;
-    this.highPrice = highPrice;
-    this.lowPrice = lowPrice;
-    this.highMessage = highMessage;
-    this.lowMessage = lowMessage;
-    updatePrice();
-  }
+	public Product(String name, int lowPrice, int highPrice, String lowMessage, String highMessage) {
+		this.name = name;
+		this.highPrice = highPrice;
+		this.lowPrice = lowPrice;
+		this.highMessage = highMessage;
+		this.lowMessage = lowMessage;
+		updatePrice();
+	}
 
-  public int getPrice() {
-    return price;
-  }
+	public int getPrice() {
+		return price;
+	}
 
-  public String getPriceValue() {
-    return CURRENCY_FORMATTER.format(price);
-  }
+	public String getPriceValue() {
+		return CURRENCY_FORMATTER.format(price);
+	}
 
-  public String getName() {
-    return name;
-  }
+	public String getName() {
+		return name;
+	}
 
-  protected String getHighMessage() {
-    return String.format(highMessage, name);
-  }
+	protected String getHighMessage() {
+		return String.format(highMessage, name);
+	}
 
-  protected String getLowMessage() {
-    return String.format(lowMessage, name);
-  }
+	protected String getLowMessage() {
+		return String.format(lowMessage, name);
+	}
 
-  @Override
-  public String toString() {
-    return getName() + " "+ getPriceValue();
-  }
+	@Override
+	public String toString() {
+		return getName() + " " + getPriceValue();
+	}
 
-  final public void updatePrice() {
-    price = lowPrice + (int) (Math.random() * ((highPrice - lowPrice) + 1));
-    message = null;
-    
-    int rand = (int) (Math.random() * EVENT_CHANCE);
-    if (rand == 1) {
-      price *= EVENT_RATE;
-      message = new Message(PRICES_HIGH, getHighMessage());
-    } else if (rand == 0) {
-      price /= EVENT_RATE;
-      message = new Message(PRICES_LOW, getLowMessage());
-    }
-  }
+	final public void updatePrice() {
+		price = lowPrice + (int) (Math.random() * ((highPrice - lowPrice) + 1));
+		message = null;
 
- public Message getMessage() {
-    return message;
-  }
+		int rand = (int) (Math.random() * EVENT_CHANCE);
+		if (rand == 1) {
+			price *= EVENT_RATE;
+			message = new Message(PRICES_HIGH, getHighMessage());
+		} else if (rand == 0) {
+			price /= EVENT_RATE;
+			message = new Message(PRICES_LOW, getLowMessage());
+		}
+	}
+
+	public Message getMessage() {
+		return message;
+	}
+
+	private Object readResolve() {
+		return new Product(name, lowPrice, highPrice);
+	}
 }
